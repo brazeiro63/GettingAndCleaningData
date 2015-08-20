d31ccc5a45b829f193ac5338f0385002476f216b
Paulo Ricardo Brazeiro de Carvalho <brazeiro63@gmail.com>
2015-08-20 16:40:54 -0300

    Script of Course project ended. Directory cleaned.

--- CourseProject/run_analysis.R
+++ CourseProject/run_analysis.R
@@ -2,65 +2,66 @@ library(dplyr)
 library(data.table)
 library(tidyr)
 
+## Uncompressing file in the working directory
+unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+
+## Defining file names to get data
+feat_fn <-  "UCI HAR Dataset/features.txt"
+train_fn <-  "UCI HAR Dataset/train/X_train.txt"
+train_sbjc_fn <-  "UCI HAR Dataset/train/subject_train.txt"
+train_actv_fn <-  "UCI HAR Dataset/train/y_train.txt"
+test_fn <-  "UCI HAR Dataset/test/X_test.txt"
+test_sbjc_fn <-  "UCI HAR Dataset/test/subject_test.txt"
+test_actv_fn <-  "UCI HAR Dataset/test/y_test.txt"
+actv_labl_fn <-  "UCI HAR Dataset/activity_labels.txt"
+
 ##Getting variable names for the train and test raw data
-feat_df <- tbl_df(fread("UCI HAR Dataset/features.txt"))
+feat_df <- tbl_df(fread(feat_fn))
 
-## Reading and preparing the train and test data frame 
-tran_df <- read.table("UCI HAR Dataset/train/X_train.txt", colClasses = "numeric", col.names = feat_df$V2)
-test_df <- read.table("UCI HAR Dataset/test/X_test.txt",   colClasses = "numeric", col.names = feat_df$V2)
+## Reading and preparing the train and test data
+tran_df   <- read.table(train_fn, colClasses = "numeric", col.names = feat_df$V2)
+tran_sbjc <- read.table(train_sbjc_fn, col.names = "subjectID")
+tran_actv <- read.table(train_actv_fn, col.names = "activityID")
+test_df   <- read.table(test_fn,  colClasses = "numeric", col.names = feat_df$V2)
+test_sbjc <- read.table(test_sbjc_fn,  col.names = "subjectID")
+test_actv <- read.table(test_actv_fn,  col.names = "activityID")
+actv_labl <- read.table(actv_labl_fn, col.names = c("id", "name"))
 
-# Subsetting train and test data with mean and standard deviation columns
-tran_df_ss <-  select(tran_df, contains("mean.."), contains("std.."), -starts_with("angle"))
-test_df_ss <-  select(test_df, contains("mean.."), contains("std.."), -starts_with("angle"))
+## Merging train and test tables, including subject and activity data
+step1 <- cbind(rbind(tran_sbjc, test_sbjc),
+               rbind(tran_actv, test_actv),
+               rbind(tran_df, test_df))
 
-## getting subjectID and activityID data for train and test
-tran_sbjc <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subjectID") 
-tran_actv <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activityID")
-test_sbjc <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subjectID") 
-test_actv <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activityID")
+## Subsetting mean and standard deviation columns
+## Changing activityID by activityName (step3)
+step2 <- step1 %>%
+    select(subjectID, activityID, contains("mean.."), contains("std.."),
+           -starts_with("angle")) %>%
+    mutate(activityName = actv_labl$name[activityID])
 
 
-## Adding subjectID, activityID, and observationType columns 
-## to both train and test table data frames
-tran_dft_ss <- tbl_df(cbind(tran_sbjc, 
-                            tran_actv, 
-                            tran_df_ss)) %>% 
-               mutate(observationType = "train")
+## Changing column names to other more descriptive
+step4 <- copy(step2)
+names(step4) <- gsub("\\.","_",gsub("\\.\\.","",names(step2)))
+tmp1 <-names(step4)
+tmp <- sapply(tmp1[3:68],function(x){x<-sub("t","time",x)})
+tmp <- sapply(tmp,function(x){x<-sub("f","frequency",x)})
+tmp <- sapply(tmp,function(x){x<-sub("Acc","Accelaration",x)})
+tmp <- sapply(tmp,function(x){x<-sub("_mean","Mean",x)})
+tmp <- sapply(tmp,function(x){x<-sub("_std","StandardDeviation",x)})
+tmp <- sapply(tmp,function(x){x<-sub("_stimed","StandardDeviation",x)})
+tmp <- sapply(tmp,function(x){x<-sub("BodyBody","Body",x)})
+tmp1[3:68]<-tmp
+names(step4) <- tmp1
 
-test_dft_ss <- tbl_df(cbind(test_sbjc, 
-                            test_actv, 
-                            test_df_ss)) %>% 
-               mutate(observationType = "test")
+## Tidying data
+step5 <- step4 %>%
+  select(subjectID, activityName,
+         timeBodyAccelarationMean_X:frequencyBodyGyroJerkMagStandardDeviation) %>%
+  gather(measureDomain, value,
+         timeBodyAccelarationMean_X:frequencyBodyGyroJerkMagStandardDeviation)%>%
+  group_by(subjectID, activityName, measureDomain) %>%
+  summarise(averageValue = mean(value))
 
-## Joining the two table data frames together
-full_dft <- tbl_df(rbind(tran_dft_ss, test_dft_ss) )
-
-## Cleaning collumn names
-names(full_dft) <- gsub("\\.","_",gsub("\\.\\.","",names(full_dft)))
-
-## Tiding the first part of the data frame
-## collumns that has three parts names
-part_1 <- full_dft %>% 
-	select(subjectID, activityID, observationType, ends_with("X"), ends_with("Y"), ends_with("Z")) %>%
-	gather( measureDomain, value, tBodyAcc_mean_X:fBodyGyro_std_Z)%>%
-	separate(measureDomain, c("measureDomainName", "measureType", "axis")) %>%
-	mutate(measureType = paste(measureType, axis, sep="-"), axis = NULL)
-
-## Tiding the second part of the data frame
-## collumns that has two parts names
-part_2 <- full_dft %>% 
-	select(subjectID, activityID, observationType, ends_with("mean"), ends_with("std")) %>%
-	gather( measureDomain, value, tBodyAccMag_mean:fBodyBodyGyroJerkMag_std) %>%
-	separate(measureDomain, c("measureDomainName", "measureType"))
-
-## Joining the final result and sorting the rows
-full_final <- rbind(part_1,part_2) %>% 
-	arrange(subjectID, activityID, observationType, measureDomainName, measureType)
-
-## Writing the result txt file
-write.table(full_final, "result.txt")
-
-## Sampling the result to console
-full_final
-tail(full_final, 10)
-
+## Writing resulting file
+write.table(step5, file = "step5ResultFile.txt", row.names = FALSE)
--- CourseProject/testeDb.R
+++
@@ -1,8 +0,0 @@
-testeDB <- function(){
-  con <-
-    dbConnect(MySQL(), db = "hg19", host = "genome-mysql.cse.ucsc.edu")
-  query <- "show databases;"
-  result <- dbGetQuery(con, query)
-  dbDisconnect(con)
-  result
-}
